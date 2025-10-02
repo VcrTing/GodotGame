75972dc173f07e52cb4316f56694eb730bed3ce0
ZengYiDan <vcrting@163.com>
2025-10-03 00:10:05 +0800

    '0.0'

---
+++ zvb-4/scripts/zombi/zero/ZeroZombiAttackArea.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public partial class ZeroZombiAttackArea : Area2D
+{
+}
---
+++ zvb-4/scripts/zombi/zero/ZeroZombiView.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public partial class ZeroZombiView : Node2D
+{
+}
